f06827aad757b723c27b14f4792c4e54df0375d3
Elliot Scott <elliotscott2111@gmail.com>
2026-07-15 13:14:45 +0100

    Added code to draw a 2D version of a tetrahedron on the renderPanel.

--- src/RenderEngine.java
+++ src/RenderEngine.java
@@ -1,9 +1,31 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Path2D;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RenderEngine {
 
     static void main(String[] args){
+        List<Triangle> tris = new ArrayList<>();
+        tris.add(new Triangle(new Vertex(100, 100, 100),
+                new Vertex(-100, -100, 100),
+                new Vertex(-100, 100, -100),
+                Color.WHITE));
+        tris.add(new Triangle(new Vertex(100, 100, 100),
+                new Vertex(-100, -100, 100),
+                new Vertex(100, -100, -100),
+                Color.RED));
+        tris.add(new Triangle(new Vertex(-100, 100, -100),
+                new Vertex(100, -100, -100),
+                new Vertex(100, 100, 100),
+                Color.GREEN));
+        tris.add(new Triangle(new Vertex(-100, 100, -100),
+                new Vertex(100, -100, -100),
+                new Vertex(-100, -100, 100),
+                Color.BLUE));
+
+
         JFrame frame = new JFrame();
         Container pane = frame.getContentPane();
         pane.setLayout(new BorderLayout());
@@ -18,15 +40,36 @@ public class RenderEngine {
 
         // Panel to display results
         JPanel renderPanel = new JPanel(){
+            @Override
             public void paintComponent(Graphics g){
                 Graphics2D g2 = (Graphics2D) g;
+
+                // clear background
                 g2.setColor(Color.BLACK);
                 g2.fillRect(0, 0, getWidth(), getHeight());
+
+                g2.translate(getWidth() / 2, getHeight() / 2);
+                g2.setColor(Color.WHITE);
+                for(Triangle t: tris){
+                    Path2D path = new Path2D.Double();
+                    path.moveTo(t.v1.x, t.v1.y);
+                    path.lineTo(t.v2.x, t.v2.y);
+                    path.lineTo(t.v3.x, t.v3.y);
+                    path.closePath();
+                    g2.draw(path);
+                }
+
+
             }
         };
 
         pane.add(renderPanel, BorderLayout.CENTER);
         frame.setSize(400, 400);
         frame.setVisible(true);
+
+
     }
+
+
+
 }
